deb6db34677defbf098447dfdce3299d262bba60
AnimewkaSes <brol0526@gmail.com>
2022-12-13 00:35:27 +0600

    Задача 34

---
+++ Ex034/Program.cs
@@ -0,0 +1,27 @@
+﻿internal class Program
+{
+private static void Main(string[] args)
+{
+Console.WriteLine("Программа для поиска количесвта четных чисел в массиве");
+Console.Write("Введите размер массива: ");
+int n = Convert.ToInt32(Console.ReadLine());
+int result = 0;
+int [] arr = new int [n];
+Random rnd = new Random();
+Console.Write("Массив: ");
+for (int i = 0; i < arr.Length; i++)
+{
+arr[i] = rnd.Next(100,999);
+Console.Write("{0} ",arr[i]);
+}
+
+for (int i = 0; i < arr.Length; i++)
+{
+if (arr[i]%2==0)
+{
+result +=1;
+}
+}
+Console.WriteLine("\nКоличество чётных чисел в массиве -> {0}",result);
+}
+}
