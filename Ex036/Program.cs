a113ca2e6584b83dedc7a997ee84bd5c4d542331
AnimewkaSes <brol0526@gmail.com>
2022-12-12 22:34:40 +0600

    Задача 36

---
+++ Ex036/Program.cs
@@ -0,0 +1,32 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.Write("Введите количество генерируемых чисел: ");
+        int n = Convert.ToInt32(Console.ReadLine());
+        int[] arr = new int[n];
+        int result = 0;
+        Random rnd = new Random();
+        Console.Write("Сгенерируемый массив: ");
+        for (int i = 0; i < arr.Length; i++)
+        {
+            arr[i] = rnd.Next(-20, 20);
+        }
+
+        for (int i = 0; i < arr.Length; i++)
+        {
+            Console.Write("{0}, ", arr[i]);
+        }
+
+        for (int i = 0; i < arr.Length; i++)
+        {
+            if(i%2!=0)
+            {
+                result +=arr[i]; 
+            }
+
+        }
+        Console.WriteLine("\nСумма элементов с нечётными индексами {0}",result);
+
+    }
+}
